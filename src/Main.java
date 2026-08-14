dbb6310306e939c542eae8e562e95a70f36c7edb
NYZ29 <u.nail2006@mail.ru>
2026-08-14 20:24:29 +0300

    Module 1 is completed

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,18 @@
-/**
- * Вычисляет факториал числа n.
- * Факториал числа n – это произведение всех натуральных чисел от 1 до n включительно.
- */
-class Factorial {
-    /// @param n
-    /// @return
-    static long factorial(int n) {
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
+class Example {
+    static void example() {
+        int x = 10;
+        if (x > 5) {
+            System.out.println("Больше пяти");
+            if (x > 8) {
+                System.out.println("Больше восьми");
+            }
         }
-        return result;
     }
 }
 
+
 public class Main {
     public static void main(String[] args) {
-        int k = 10;
-        System.out.println(Factorial.factorial(k));
+        Example.example();
     }
 }
