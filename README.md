a3a14e0e2c3d2c6eeaafcb81c56fbf2fbf21ea92
NYZ29 <u.nail2006@mail.ru>
2026-08-16 20:53:22 +0300

    Some type of break before more complex material, you know

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Module 1 is completed! ^_^ Module 2 is completed :-)
+### Module 1 is completed! ^_^
+
+### Module 2 is completed :-)
+
+### Skipped modules 3-4. They are boring. Or there are reviewed material ¬_¬
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-
+import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args) {
         System.out.println();
-        
+
+        int nums[] = {5, 3, 8, 1, 9, 3};
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
