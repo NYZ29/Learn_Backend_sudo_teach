928fe199375488847a827b3c3d6dbda388f9c54a
NYZ29 <u.nail2006@mail.ru>
2026-08-18 16:19:11 +0300

    Matrix Multiplication

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 
 
 class Practice {
+    /// @param matrix
     static void rotateMatrix90DegreesClockwise(int @NotNull [][] matrix) {
         int n = matrix.length;
         int[][] rotated = new int[n][n];
@@ -17,6 +19,7 @@ class Practice {
         System.out.println("Повёрнутая: " + Arrays.deepToString(rotated));
     }
 
+    /// @param matrix
     static void findSumOnDiags(int @NotNull [][] matrix) {
         int mainDiagSum = 0;
         int antiDiagSum = 0;
@@ -30,6 +33,8 @@ class Practice {
         System.out.println("Побочная диагональ: " + antiDiagSum);
     }
 
+    /// @param matrix
+    /// @param target
     static void findCountOfOccurrences(int @NotNull [][] matrix, int target) {
         int count = 0;
 
@@ -44,7 +49,12 @@ class Practice {
         System.out.println("Число " + target + " встречается " + count + " раз");
     }
 
+    /// @param n
     static void createIdentityMatrix(int n) {
+        if (n <= 0) {
+            System.out.println("Не существует матрицы с такой размерностью");
+            return;
+        }
         int[][] identity = new int[n][n];
 
         for (int i = 0; i < n; i++) {
@@ -54,6 +64,7 @@ class Practice {
         System.out.println(Arrays.deepToString(identity));
     }
 
+    /// @param matrix
     static void findWithMaxSum(int @NotNull [][] matrix) {
         int maxSum = 0;
         int maxRowIndex = 0;
@@ -72,6 +83,7 @@ class Practice {
         System.out.println("Строка с индексом " + maxRowIndex + " имеет наибольшую сумму: " + maxSum);
     }
 
+    /// @param figure
     static void dimension3D(int @NotNull [][][] figure) {
         int length = figure.length;
         int width = figure[0].length;
@@ -91,8 +103,44 @@ class Practice {
         System.out.println(Arrays.deepToString(figure));
     }
 
-    static void matrixMultiplication(int @NotNull [][] matrix1, int @NotNull [][] matrix2) {
-        
+    /// @param matrix1
+    /// @param matrix2
+    /// @return
+    static int @Nullable [][] matrixMultiplication(int @NotNull [][] matrix1, int @NotNull [][] matrix2) {
+        int len1 = matrix1.length;
+        int wid1 = matrix1[0].length;
+        int len2 = matrix2.length;
+        int wid2 = matrix2[0].length;
+
+        if (wid1 != len2) {
+            int[][] m3 = {{0}};
+            return m3;
+        }
+
+        int[][] matrix3 = new int[len1][wid2];
+
+        for (int i = 0; i < len1; i++) {
+            for (int j = 0; j < wid2; j++) {
+                for (int k = 0; k < wid1; k++) {
+                    matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        return matrix3;
+    }
+
+    /// @param matrix1
+    /// @param matrix2
+    static void showResultMatrix(int @NotNull [][] matrix1, int @NotNull [][] matrix2) {
+        int[][] matrix3 = matrixMultiplication(matrix1, matrix2);
+        if (matrix1[0].length != matrix2.length) {
+            System.out.println("Невозможно умножить! Кол-во столбцов матрицы1 != кол-во строк матрицы2 (Длина строки матрицы1 != длина столбца матрицы2)");
+            return;
+        }
+        System.out.println("M1 (" + matrix1.length + "×" + matrix1[0].length + "): " + Arrays.deepToString(matrix1));
+        System.out.println("M2 (" + matrix2.length + "×" + matrix2[0].length + "): " + Arrays.deepToString(matrix2));
+        System.out.println("M1×M2 (" + matrix3.length + "×" + matrix3[0].length + "): " + Arrays.deepToString(matrix3));
     }
 }
 
@@ -135,5 +183,30 @@ public class Main {
 
         int[][][] parallelepiped = new int[3][4][5];
         Practice.dimension3D(parallelepiped);
+        System.out.println();
+
+        int[][] A = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        int[][] B = {
+                {7, 8},
+                {9, 10},
+                {11, 12}
+        };
+
+        int[][] C = {
+                {13, 14}
+        };
+
+        Practice.showResultMatrix(A, B);
+        System.out.println();
+        Practice.showResultMatrix(B, A);
+        System.out.println();
+        Practice.showResultMatrix(A, C);
+        System.out.println();
+        Practice.showResultMatrix(C, A);
+        System.out.println();
     }
 }
