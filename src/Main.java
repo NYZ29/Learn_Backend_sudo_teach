7e0d8e701c5ed1b944878d0ce31782e09f4c16e9
NYZ29 <u.nail2006@mail.ru>
2026-08-21 20:11:05 +0300

    Изучение работы с args, терминалом, а ещё за исключениями (Learning how to work with args, terminal, and other exceptions)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println();
-
+class WorkWithArgs {
+    static void argsDemo(String[] args) {
         // Длина массива
         System.out.println("Аргументов: " + args.length);
 
@@ -10,7 +8,7 @@ public class Main {
             System.out.println("Первый: " + args[0]);
         }
         if (args.length > 1) {
-            System.out.println("Второй: " + args[0]);
+            System.out.println("Второй: " + args[1]);
         }
 
         // Последний аргумент
@@ -22,5 +20,66 @@ public class Main {
         for (String arg : args) {
             System.out.println("Аргумент: " + arg);
         }
+    }
+
+    static void argsMethods(String[] args) {
+        // Проверка на пустоту
+        if (args.length == 0) {
+            System.out.println("Аргументы не переданы!");
+            return;
+        }
+
+        // Цикл for с индексом
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(i + ": " + args[i]);
+        }
+
+        // for-each
+        for (String arg : args) {
+            System.out.println(arg.toUpperCase());
+        }
+    }
+
+    static void parseIntLocal(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Передайте число!");
+            return;
+        }
+
+        try {
+            int number = Integer.parseInt(args[0]);
+            System.out.println("Вы передали число: " + number);
+            System.out.println("Удвоенное: " + (number * 2));
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка: '" + args[0] + "' не является целым числом.");
+        }
+    }
+
+    static void parseDouble(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Передайте число!");
+            return;
+        }
+
+        double number = Double.parseDouble(args[0]);
+        System.out.println("Число: " + number);
+        System.out.println("Квадрат: " + (number * number));
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        WorkWithArgs.argsDemo(args);
+
+        System.out.println();
+        WorkWithArgs.argsMethods(args);
+
+        System.out.println();
+        WorkWithArgs.parseIntLocal(args);
+
+        System.out.println();
+        WorkWithArgs.parseDouble(args);
     }
 }
