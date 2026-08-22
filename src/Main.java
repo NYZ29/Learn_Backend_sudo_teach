3ff7a97dffc4d010e75204fa5d6eef5e55250831
NYZ29 <u.nail2006@mail.ru>
2026-08-22 23:14:59 +0300

    Пожалуй, конец с обработчиком ввода чисел / Perhaps the end of the number entry handler

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,28 @@
 class WorkWithArgs {
-    static void parseIntLocal(String[] args) {
+    static int parseIntArg(String[] args) {
         if (args.length < 1) {
-            System.out.println("Передайте число! (Local)");
-            return;
-        }
-
-        try {
-            int number = Integer.parseInt(args[0]);
-            System.out.println("Вы передали число (Local): " + number);
-            System.out.println("Удвоенное (Local): " + (number * 2));
-        } catch (NumberFormatException e) {
-            System.out.println("Ошибка (Local): '" + args[0] + "' не является целым числом.");
-        }
-    }
-
-    static int parseIntArg(String[] args) throws NumberFormatException {
-        if (args.length < 1) {
-            throw new IllegalArgumentException("Нет аргументов (Arg)");
+            throw new IllegalArgumentException("Нет аргументов");
         }
         return Integer.parseInt(args[0]);
     }
 
-    static int parseIntArgMine(String[] args) {
+    static double parseDoubleArg(String[] args) {
         if (args.length < 1) {
-            throw new IllegalArgumentException("Нет аргументов (ArgMine)");
-        }
-
-        try {
-            return Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Первый аргумент должен быть целым числом (ArgMine)", e);
-        }
-    }
-
-    static void parseDoubleLocal(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Передайте число! (LocalDouble)");
-            return;
-        }
-
-        try {
-            double number = Double.parseDouble(args[0]);
-            System.out.println("Число (LocalDouble): " + number);
-            System.out.println("Квадрат (LocalDouble): " + (number * number));
-        } catch (NumberFormatException e) {
-            System.out.println("Ошибка: число " + args[0] + " не является дробным. (LocalDouble)");
-        }
-    }
-
-    static double parseDoubleArg(String[] args) throws NumberFormatException {
-        if (args.length < 1) {
-            throw new IllegalArgumentException("Нет аргументов (DoubleArg)");
+            throw new IllegalArgumentException("Нет аргументов");
         }
         return Double.parseDouble(args[0]);
     }
+}
 
-    static double parseDoubleArgMine(String[] args) {
-        if (args.length < 1) {
-            throw new IllegalArgumentException("Нет аргументов (DoubleArgMine)");
-        }
 
-        try {
-            return Double.parseDouble(args[0]);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Первый аргумент должен быть дробным числом (DoubleArgMine)", e);
+class ExceptionHandler {
+    static void handle(Exception e, String context) {
+        if (e instanceof NumberFormatException) {
+            System.out.println("Ошибка формата числа: (" + context + "): не является целым числом.");
+        } else if (e instanceof IllegalArgumentException) {
+            System.out.println("Ошибка аргумента (" + context + "): " + e.getMessage());
+        } else {
+            System.out.println("Неожиданная ошибка: (" + context + "): " + e.getMessage());
         }
     }
 }
@@ -72,52 +31,23 @@ class WorkWithArgs {
 public class Main {
     public static void main(String[] args) {
         System.out.println();
-        WorkWithArgs.parseIntLocal(args);
 
-        System.out.println();
+        // int
         try {
             int number = WorkWithArgs.parseIntArg(args);
-            System.out.println("Вы передали число (Arg): " + number);
-            System.out.println("Удвоенное (Arg): " + (number * 2));
-        } catch (NumberFormatException e) {
-            System.out.println("Ошибка: первый аргумент должен быть целым числом. (Arg)");
+            System.out.println("Вы передали число: " + number);
+            System.out.println("Удвоенное: " + (number * 2));
         } catch (IllegalArgumentException e) {
-            System.out.println("Ошибка: передайте хотя бы один аргумент. (Arg)");
-        }
-        System.out.println("Программа продолжается...");
-
-        System.out.println();
-        try {
-            int number = WorkWithArgs.parseIntArgMine(args);
-            System.out.println("Вы передали число (ArgMine): " + number);
-            System.out.println("Удвоенное (ArgMine): " + (number * 2));
-        } catch (IllegalArgumentException e) {
-            System.out.println("Ошибка ввода (ArgMine): " + e.getMessage());
+            ExceptionHandler.handle(e, "parseIntArg");
         }
 
-
-        System.out.println();
-        WorkWithArgs.parseDoubleLocal(args);
-
-        System.out.println();
+        // double
         try {
             double number = WorkWithArgs.parseDoubleArg(args);
-            System.out.println("Число (DoubleArg): " + number);
-            System.out.println("Квадрат (DoubleArg): " + (number * number));
-        } catch (NumberFormatException e) {
-            System.out.println("Ошибка: первый аргумент должен быть дробным числом. (DoubleArg)");
+            System.out.println("Число: " + number);
+            System.out.println("Квадрат: " + (number * number));
         } catch (IllegalArgumentException e) {
-            System.out.println("Ошибка: передайте хотя бы один аргумент. (DoubleArg)");
-        }
-        System.out.println("Программа продолжается...");
-
-        System.out.println();
-        try {
-            double number = WorkWithArgs.parseDoubleArgMine(args);
-            System.out.println("Число (DoubleArgMine): " + number);
-            System.out.println("Квадрат (DoubleArgMine): " + (number * number));
-        } catch (IllegalArgumentException e) {
-            System.out.println("Ошибка ввода (DoubleArgMine): " + e.getMessage());
+            ExceptionHandler.handle(e, "parseDoubleArg");
         }
     }
 }
