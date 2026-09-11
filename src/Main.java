7652cdf0d3cd7f5b7c856576a33313cf6be8f0cc
NYZ29 <u.nail2006@mail.ru>
2026-09-11 08:22:02 +0300

    Очистка / Cleaning

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,8 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-class ListUtils {
-    static <T> List<T> filter(List<T> list, Condition<T> condition) {
-        List<T> result = new ArrayList<>();
-        for (T item : list) {
-            if (condition.check(item)) {
-                result.add(item);
-            }
-        }
-        return result;
-    }
-
-    interface Condition<T> {
-        boolean check(T item);
-    }
-}
 
 
 public class Main {
     public static void main(String[] args) {
         System.out.println();
-
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-        List<Integer> evenNumbers = ListUtils.filter(numbers, item -> item % 2 == 0);
-
-        System.out.println("Чётные числа: " + evenNumbers);
     }
 }
